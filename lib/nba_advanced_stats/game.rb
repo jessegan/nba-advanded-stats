a8e3383d22a0ef98cf0838ca4e71da2aab9fe87a
jessegan <jesse13579@gmail.com>
2020-06-16 12:07:58 -0400

    Added basic constructors for game.rb and implemented into api.rb

--- lib/nba_advanced_stats/game.rb
+++ lib/nba_advanced_stats/game.rb
@@ -1,10 +1,27 @@
 class NbaAdvancedStats::Game 
 
-    
+    attr_accessor :date,:home_team,:away_team,:home_score,:away_score
 
+    @@all = []
+
+    # Constructors
     def initialize(date:,home_team:,away_team:,home_score:,away_score:)
+        @date=date
+        @home_team=home_team
+        @away_team=away_team
+        @home_score=home_score
+        @away_score=away_score
+        self.save
+    end
 
-        
+    # Class getter
+    def self.all
+        @@all
+    end
+
+    # instance methods
+    def save
+        @@all << self
     end
     
 end
